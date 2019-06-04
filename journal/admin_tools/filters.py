8eb7d35278de67acebdfab97b2820a960b7b5b0e
Kostiantyn Minkov <fokkiminkov@gmail.com>
2019-06-05 00:33:22 +0300

    E

--- journal/admin_tools/filters.py
+++ journal/admin_tools/filters.py
@@ -4,6 +4,36 @@ from office.models import Subject
 from journal.models import Rating
 
 
+class MixinSubjectListFilters(admin.SimpleListFilter):
+    title = 'Предмети'
+    parameter_name = 'subjects'
+    default_value = None
+    queryset_all = Subject.objects.all()
+    queryset_filter = Subject.objects.filter()
+
+    def lookups(self, request, model_admin):
+        list_of_species = []
+        if request.user.is_superuser:
+            queryset = Subject.objects.all()
+        else:
+            queryset = Subject.objects.filter(rating__marks__author=request.user)
+
+        for species in queryset:
+            list_of_species.append(
+                (str(species.id), f'{species.group.number} : {species.name}')
+            )
+        return sorted(list_of_species, key=lambda tp: tp[1])
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            return queryset.filter(rating__subject_id=self.value())
+        return queryset.all()
+
+    def value(self):
+        value = super().value()
+        return value
+
+
 class TeacherSubjectsListFilter(admin.SimpleListFilter):
     """ Filter by group number for marks admin panel"""
     title = 'предметами'
